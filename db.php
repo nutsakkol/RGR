386675cb936494bff56de146fba7da12576d4b55
nutsakkol <t.nuts@mail.ru>
2013-12-19 00:49:06 +0700

    create new User and write in database

---
+++ db.php
@@ -0,0 +1,3 @@
+<?php
+$connect = mysql_connect('localhost', 'arex', '123456')or die(mysql_error());
+$connect = mysql_select_db('rgr');
--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 include('db.php');
 
-echo 'First Commit';
+session_start();
+
+if (isset($_POST['Registration'])) {
+    $password = md5($_POST['Registration']['password']);
+    $query = mysql_query("INSERT INTO users(name,email,password)
+                    VALUES('{$_POST['Registration']['name']}',
+                            '{$_POST['Registration']['email']}',
+                            '$password')") or die(mysql_error());
+    echo 'success';
+}
 ?>
+
+<form method="post" action="">
+    <input type="text" name="Registration[name]" placeholder="name" required/><br>
+    <input type="text" name="Registration[email]" placeholder="email" required/><br>
+    <input type="password" name="Registration[password]" placeholder="password" required/><br>
+    <input type="submit" name="submit" value="Register"/>
+</form>
